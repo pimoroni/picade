1225f7220f24f2a676b409e3f1b3735eff38d94e
lowfatcode <jon@magicnumber.co.uk>
2013-09-28 11:11:26 +0100

    Switched to struct to store input information

--- software/picade-controller-arduino/picade-controller.c
+++ software/picade-controller-arduino/picade-controller.c
@@ -1,11 +1,24 @@
-// key codes to emmit when input detected
-char keys[]  = {KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_RIGHT_ARROW, KEY_DOWN_ARROW, KEY_LEFT_CTRL, KEY_LEFT_ALT, '1', '5', '6', KEY_ESC, KEY_RETURN};
-// 0 = PORTB, 1 = PORTF, 2 = PORTD
-char ports[] = {0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2};
-// bitmask to test the port against
-char masks[] = {B00000001, B00000010, B00000100, B00001000, B00000001, B00000010, B00010000, B00100000, B01000000, B00000100, B00001000};
-// current state (0 = unpressed, 1 = pressed)
-boolean pressed[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+typedef struct
+{
+  char key;        // which keycode to emit when input is pressed
+  int  port;       // which port is this input on? 0 = PORTB, 1 = PORTF, 2 = PORTD
+  char mask;       // bitmask for the input on the port
+  boolean pressed; // is this input current pressed?
+} input;
+
+input inputs[] = {
+  { KEY_LEFT_ARROW,   0, B00000001, 0 },
+  { KEY_UP_ARROW,     0, B00000010, 0 },
+  { KEY_RIGHT_ARROW,  0, B00000100, 0 },
+  { KEY_DOWN_ARROW,   0, B00001000, 0 },
+  { KEY_LEFT_CTRL,    1, B00000001, 0 },
+  { KEY_LEFT_ALT,     1, B00000010, 0 },
+  { '1',              1, B00010000, 0 },
+  { '5',              1, B00100000, 0 },
+  { '6',              1, B01000000, 0 },
+  { KEY_ESC,          2, B00000100, 0 },
+  { KEY_RETURN,       2, B00001000, 0 }
+};
 
 void setup() {
   /*
@@ -24,20 +37,20 @@ void loop() {
   char pinStates[3] = {PINB, PINF, PIND}; // read the current port states and store in 0, 1, 2 indexed array to match our port ids above
   
   // loop through each input
-  for(int i = 0; i < sizeof(keys) / sizeof(char); i++)
+  for(int i = 0; i < sizeof(inputs) / sizeof(input); i++)
   {
     // test for current state of this input
-    boolean test = ~pinStates[ports[i]] & masks[i];
+    boolean test = ~pinStates[inputs[i].port] & inputs[i].mask;
     
-    if(test != pressed[i]) // has this input changed state since the last time we checked?
+    if(test != inputs[i].pressed) // has this input changed state since the last time we checked?
     {
       changed = true; // something changed, we should remember that
-      pressed[i] = test; // update our state map so we know what's happening with this key in future
+      inputs[i].pressed = test; // update our state map so we know what's happening with this key in future
       
       if(test) // send the key press or release event
-        Keyboard.press(keys[i]);
+        Keyboard.press(inputs[i].key);
       else
-        Keyboard.release(keys[i]);
+        Keyboard.release(inputs[i].key);
     }
   }
   
