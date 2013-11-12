faa868729da3a83315a3eb89f6e2f0965b2387ae
Jonathan Williamson <jon@pimoroni.com>
2013-11-12 12:43:49 +0000

    Adding more button mappins

--- software/picade-controller-arduino/picade-controller.c
+++ software/picade-controller-arduino/picade-controller.c
@@ -7,17 +7,24 @@ typedef struct
 } input;
 
 input inputs[] = {
-  { KEY_LEFT_ARROW,   0, B00000001, 0 },
-  { KEY_UP_ARROW,     0, B00000010, 0 },
-  { KEY_RIGHT_ARROW,  0, B00000100, 0 },
-  { KEY_DOWN_ARROW,   0, B00001000, 0 },
-  { KEY_LEFT_CTRL,    1, B00000001, 0 },
-  { KEY_LEFT_ALT,     1, B00000010, 0 },
-  { '1',              1, B00010000, 0 },
-  { '5',              1, B00100000, 0 },
-  { '6',              1, B01000000, 0 },
-  { KEY_ESC,          2, B00000100, 0 },
-  { KEY_RETURN,       2, B00001000, 0 }
+  { KEY_UP_ARROW,      0, B00000001, 0 },
+  { KEY_DOWN_ARROW,    0, B00000010, 0 },
+  { KEY_LEFT_ARROW,    0, B00000100, 0 },
+  { KEY_RIGHT_ARROW,   0, B00001000, 0 },
+
+  /* port F */
+  { KEY_LEFT_CTRL,     1, B00000001, 0 },
+  { KEY_LEFT_ALT,      1, B00000010, 0 },
+  { KEY_LEFT_CTRL,     1, B00010000, 0 },
+  { KEY_LEFT_ALT,      1, B00100000, 0 },
+  { KEY_LEFT_CTRL,     1, B01000000, 0 },
+  { KEY_LEFT_ALT,      1, B10000000, 0 },
+
+  /* port d */
+  { '5',               2, B00000001, 0 }, // 0
+  { '1',               2, B00000010, 0 }, // 1
+  { KEY_ESC,           2, B00000100, 0 }, // 2
+  { KEY_RETURN,        2, B00001000, 0 }  // 3
 };
 
 void setup() {
@@ -35,25 +42,25 @@ void loop() {
   */
   boolean changed = false;  // has any input changed?
   char pinStates[3] = {PINB, PINF, PIND}; // read the current port states and store in 0, 1, 2 indexed array to match our port ids above
-  
+
   // loop through each input
   for(int i = 0; i < sizeof(inputs) / sizeof(input); i++)
   {
     // test for current state of this input
     boolean test = ~pinStates[inputs[i].port] & inputs[i].mask;
-    
+
     if(test != inputs[i].pressed) // has this input changed state since the last time we checked?
     {
       changed = true; // something changed, we should remember that
       inputs[i].pressed = test; // update our state map so we know what's happening with this key in future
-      
+
       if(test) // send the key press or release event
         Keyboard.press(inputs[i].key);
       else
         Keyboard.release(inputs[i].key);
     }
   }
-  
+
   if(changed) // if an input changed we'll add a small delay to avoid "bouncing" of the input
     delay(50);
 }
